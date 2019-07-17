5c66e65cb963b8dfcf238ca1fc241bd83be2529e
Sue <sue.li@post.harvard.edu>
2019-07-17 15:43:15 +0200

    not quite there with new css...

--- client/src/components/wheel.js
+++ client/src/components/wheel.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import './wheel.css';
+import './wheel.scss';
 import { wheelColors } from './../config.js';
 
-const style = (degrees, color, index) => ({
-	transform: `rotate(${degrees}deg)`,
-	'WebkitTransform': `rotate(${degrees}deg)`,
-	'MozTransform': `rotate(${degrees}deg)`,
-	'OTransform': `rotate(${degrees}deg)`,
-	'msTransform': `rotate(${degrees}deg)`,
-  'borderColor': `${color} transparent`,
-  'zIndex': 100 - index	
+const pie = (degree, color, index) => ({
+  'backgroundColor': color,
+  'transform': `rotate(${degree*(index)}deg)`,
+  'transition': 'all 1s',
+  'position': 'absolute',
+  'width': '500px',
+  'height': '500px',
+  'borderRadius': '50%',
+  'clip': `rect(0px, ${100/(index+1)}%, ${100/(index+1)}%, ${index}px)`
+})
+
+const pieSlice = (degree) => ({
+  transform: `rotate(${degree}deg)`
 })
 
 function getColor(index) {
@@ -19,39 +24,23 @@ function getColor(index) {
 function renderWheel(participants) {
   return participants.map((participant, index, allParticipants) => {
     const color = getColor(index);
-    const degree = index / allParticipants.length * 360
+    const degree = (index / allParticipants.length) * 360;
+    console.log('index', index)
+    console.log('degree', degree)
     return (
-      <div className="sec" key={participant.name} style={style(degree, color, index)}><span className="name">{participant.name}</span></div>
+      <div key={participant.name} style={pieSlice(degree)} className="hold">
+        {/* <div key={participant.name} id={`pieSlice${index+1}`} style={pieSlice(index, degree)} className="hold"> */}
+        <div style={pie(degree, color, index)}></div>
+        {/* <div className="pie"></div> */}
+      </div>
     )
   })
 }
 
-const spinStyle = (winnerIndex, participants) => {
-  const degree = winnerIndex >= 0 ? -((winnerIndex / participants.length * 360) + 360 * 6) : 0
-  return {
-    transform: `rotate(${degree}deg)`
-  }
-}
-
-const Wheel = ({participants, winnerIndex, ...props}) => {
+let Wheel = ({participants, winnerIndex, ...props}) => {
   return (
-    <div id="wrapper"> 
-      <div id="wheel">
-        <div style={spinStyle(winnerIndex, participants)} id="inner-wheel">
-          {renderWheel(participants)}
-        </div>       
-        
-        <div onClick={props.generateWinner} id="spin">
-          <div id="inner-spin">
-            <img id="logo" alt="logo" src="hackages-teal.svg"/>
-          </div>
-        </div>
-          
-        <div id="shine"></div>
-      </div>
-
-      <div id="txt"></div>
-      
+    <div className="pie-container"> 
+      {renderWheel(participants)}
     </div>
   )
 }
